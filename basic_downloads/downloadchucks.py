52bec0d05c08561c4fdb4c40eb8105a93a49a3ae
Gui binjie <jackeyz2020@sohu.com>
2020-08-22 14:13:17 +0800

    更新 downloadchucks.py
    Write chucks in file

--- basic_downloads/downloadchucks.py
+++ basic_downloads/downloadchucks.py
@@ -4,3 +4,12 @@
 @by Gui binjie
 #from "downloadchucks.py"get file chucks and write in file
 """
+import basic_downloads.getchucks
+
+def writefile(url,dir):
+    c=open(dir,"wb")
+    s=basic_downloads.getchucks.get_download_chucks(url=url)
+    for y in s[1]:
+        f=s[0][y]
+        c.write(f)
+    c.close()
